d3532d813ae40b3687855206ea45b1d126c40cff
almaz8811 <almaz8811@yandex.ru>
2024-02-22 15:47:17 +0300

    Добавил FSM

--- filters/filters.py
+++ filters/filters.py
@@ -7,3 +7,20 @@ class IsBankHandler(BaseFilter):
     async def __call__(self, message: Message) -> bool:
         print(message.text in bank_list.values())
         return message.text in bank_list.values()
+
+class IsDigitalFloat(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        print(message.text.isdigit())
+        return message.text.isdigit()
+
+
+def is_number(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+a = '123.456'
+print(is_number(a))
+
+True
--- handlers/handlers.py
+++ handlers/handlers.py
@@ -30,7 +30,7 @@ class FSMFillForm(StatesGroup):
 @router.message(CommandStart(), StateFilter(default_state))
 async def process_start_command(message: Message):
     # Передать весь словарь bank_list с кнопками
-    await message.answer(text='Внести остатки', reply_markup=keyboard_start)
+    await message.answer(text='Нажмите кнопку выбора филиала', reply_markup=keyboard_start)
 
 
 # Этот хэндлер будет срабатывать на команду /cancel в любых состояниях
@@ -39,7 +39,7 @@ async def process_start_command(message: Message):
 async def process_cancel_command_state(message: Message, state: FSMContext):
     await message.answer(text='Вы отменили ввод данных.\n\n'
                               'Чтобы снова перейти к заполнению - выберите филиал.',
-                         reply_markup=keyboard_bank)
+                         reply_markup=keyboard_start)
     # Сброс состояния
     await state.clear()
 
@@ -47,13 +47,60 @@ async def process_cancel_command_state(message: Message, state: FSMContext):
 # Этот хэндлер будет срабатывать на команду /cancel в состоянии по-умолчанию
 # и сообщать, что эта команда доступна в машине состояний
 @router.message(F.text == 'Отмена', StateFilter(default_state))
-async def process_cancel_command_state(message: Message, ):
+async def process_cancel_command_state(message: Message):
     await message.answer(text='Отменять нечего. Вы не вводили данных.\n\n'
                               'Чтобы перейти к заполнению - выберите филиал.',
-                         reply_markup=keyboard_bank)
+                         reply_markup=keyboard_start)
 
 
-@router.message(F.text == 'test')
+# Этот хэндлер будет срабатывать на текст "Выбрать филиал"
+# и переводить бота в состояние ожидания выбора филиала
+@router.message(F.text == 'Выбрать филиал', StateFilter(default_state))
+async def process_select_bank(message: Message, state: FSMContext):
+    await message.answer(text='Выберите филиал', reply_markup=keyboard_bank)
+    # Устанавливаем состояние ожидания выбора филиала
+    await state.set_state(FSMFillForm.bank_name)
+
+
+# Этот хэндлер будет срабатывать, если выбран корректный филиал
+# и переводить в состояние ожидании ввода остатков по кассе
+@router.message(StateFilter(FSMFillForm.bank_name), IsBankHandler())
+async def process_bank_sent(message: Message, state: FSMContext):
+    # Сохраняем выбранный банк в хранилище по ключу "bank"
+    await state.update_data(bank=message.text)
+    await message.answer(text=f'Введите остатки по кассе для филиала {message.text}', reply_markup=keyboard_cancel)
+    # Устанавливаем состояние ожидания ввода остатков по кассе
+    await state.set_state(FSMFillForm.balance_cash)
+
+
+# Это хэндлер будет срабатывать, если во время выбора филиала будет введено что-то некорректное
+@router.message(StateFilter(FSMFillForm.bank_name))
+async def warning_not_bank(message: Message):
+    await message.answer(text=f'Филиал {message.text} не найден. Попробуйте выбрать другой филиал.',
+                         reply_markup=keyboard_cancel)
+
+
+# Этот хэндлер будет срабатывать, если введены корректные остатки по кассе
+# и переводить в состояние ожидания ввода остатков по банку
+@router.message(StateFilter(FSMFillForm.balance_cash), F.text.isdigit())
+async def process_cash_sent(message: Message, state: FSMContext):
+    # Считываем название филиала из хранилища машины состояний
+    data = await state.get_data()
+    # Сохраняем остатки по кассе в хранилище по ключу "cash"
+    await message.answer(text=f'Введите остатки по банку для филиала {data.get('bank')}',
+                         reply_markup=keyboard_cancel)
+    # Устанавливаем состояние ожидания ввода остатков по банку
+    await state.set_state(FSMFillForm.balance_bank)
+
+
+# Это хэндлер будет срабатывать, если во время ввода остатков по кассе будет введено что-то некорректное
+@router.message(StateFilter(FSMFillForm.balance_cash))
+async def warning_not_cash(message: Message):
+    await message.answer(text=f'Неверные данные: {message.text}. Введите сумму остатков по кассе.',
+                         reply_markup=keyboard_cancel)
+
+
+@router.message(F.text == 'Выбрать филиал', StateFilter(default_state))
 async def bank_command(message: Message):
     await message.answer(text='Вы отменили ввод данных\n\n'
                               'Чтобы снова перейти к заполнению - выберите филиал',
